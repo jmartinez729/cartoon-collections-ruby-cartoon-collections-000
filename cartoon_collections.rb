aae76c27d887d0865af0d759f347acbf9aec8f2c
John Martinez <jmartinez729@gmail.com>
2018-09-05 20:37:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,17 +9,23 @@ def roll_call_dwarves(dwarf_names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  upcase_planeteer_calls = []
+  # upcase_planeteer_calls = []
   
-  planeteer_calls.each do |element|
-    element = element.capitalize + "!"
-    upcase_planeteer_calls << element
-  end 
+  # planeteer_calls.each do |element|
+  #   element = element.capitalize + "!"
+  #   upcase_planeteer_calls << element
+  # end 
   
-  return upcase_planeteer_calls
+  # return upcase_planeteer_calls
+  
+  planeteer_calls.map { |call| call = call.capitalize + "!" }
 end
 
 def long_planeteer_calls(calls)
+<<<<<<< HEAD
+=======
+  is_long = nil 
+>>>>>>> 4f7493b3c56d1921767514c7d349a234295bca74
   
   calls.any? do |call|
     if call.length > 4
@@ -28,19 +34,39 @@ def long_planeteer_calls(calls)
       return false 
     end 
   end 
+<<<<<<< HEAD
+=======
+  
+  return is_long
+>>>>>>> 4f7493b3c56d1921767514c7d349a234295bca74
 end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+<<<<<<< HEAD
   cheese_return = nil
   
   strings.find do |object|
     cheese_types.find do |cheese|
       if object == cheese
         cheese_return = object
+=======
+  contains_cheese = nil
+  
+  strings.each do |object|
+    cheese_types.each do |cheese|
+      if object == cheese
+        return object
+        return true 
+>>>>>>> 4f7493b3c56d1921767514c7d349a234295bca74
       end 
     end 
   end 
   
+<<<<<<< HEAD
   return cheese_return
 end
+=======
+  return contains_cheese
+end
+>>>>>>> 4f7493b3c56d1921767514c7d349a234295bca74
